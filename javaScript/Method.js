c13b9a7dee1e21092d0d78464eacc0d29cadf050
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-07-07 22:56:15 +0530

    Create a Method on an Object

---
+++ javaScript/Method.js
@@ -0,0 +1,19 @@
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + duck.name + ".";}
+};
+duck.sayName();
+// Returns "The name of this duck is Aflac."
+
+
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {
+    return "This dog has " + dog.numLegs + " legs.";
+  }
+};
+
+dog.sayLegs();
