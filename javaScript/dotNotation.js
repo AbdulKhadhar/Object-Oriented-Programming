ec5123ce258c3be181c9ea8f0ff624360d0865f0
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-07-07 22:48:26 +0530

    Use Dot Notation to Access the Properties

---
+++ javaScript/dotNotation.js
@@ -0,0 +1,13 @@
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+// This prints "Aflac" to the console
+
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+console.log(dog.name,dog.numLegs)
