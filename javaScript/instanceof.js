c5870e45a1b5cf312911b8efefc6768d7d87e528
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-07-08 09:35:27 +0530

    Verify an Object's Constructor with instanceof

--- javaScript/instanceof.js
+++ javaScript/instanceof.js
@@ -16,3 +16,11 @@ let canary = {
 };
 
 canary instanceof Bird; // => false
+
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(4);
+myHouse instanceof House;
