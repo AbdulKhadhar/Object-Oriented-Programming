5a349d2bf4d65504eb603fe2422c8e118216208b
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-07-21 23:03:11 +0530

    Immediately Invoked Function Expression (IIFE)
    Understand the Immediately Invoked Function Expression (IIFE)

---
+++ javaScript/IIFE.js
@@ -0,0 +1,10 @@
+(function () {
+  console.log("Chirp, chirp!");
+})(); // this is an anonymous function expression that executes right away
+// Outputs "Chirp, chirp!" immediately
+
+
+(function makeNest() {
+  console.log("A cozy nest is ready");
+})
+
